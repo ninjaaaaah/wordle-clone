bd10ec03c8c9f81b58cd91ff8a1ad3c50ad4c5b9
Jarred Luzada <jarredluzada@gmail.com>
2022-07-31 22:11:03 +0800

    Deploy gh-pages

---
+++ _next/static/pOvYtnUrpcx-H4INJUl10/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/532-8149080954cd1ffd.js","static/chunks/pages/index-0984ff3c0edafa37.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
