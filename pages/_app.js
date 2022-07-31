92a6b7c07ff97e663a09781b8b9e38dfb7b72576
Jarred Luzada <jarredluzada@gmail.com>
2022-07-31 21:43:50 +0800

    Added build script.

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,12 @@ import './style.css';
 import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }) {
-    return (
-        <>
-            <Head>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <Component {...pageProps} />
-        </>
-    );
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
