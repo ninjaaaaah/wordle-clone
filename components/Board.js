817ffa98977717d5918a04b71c635a8906b7343f
Jarred Luzada <jarredluzada@gmail.com>
2022-07-31 22:11:27 +0800

    Fixed error.

--- components/Board.js
+++ components/Board.js
@@ -1,76 +1,76 @@
 import { motion } from 'framer-motion';
 
 function Tile(props) {
-    return (
-        <div
-            className="tile"
-            style={{ '--index': props.index }}
-            id={
-                props.activeRow == props.row && props.text.length > props.index
-                    ? 'active'
-                    : props.activeRow > props.row
-                    ? props.try[props.index] == props.word[props.index]
-                        ? 'green'
-                        : props.word.includes(props.try[props.index])
-                        ? 'yellow'
-                        : 'gray'
-                    : {}
-            }
-        >
-            {props.activeRow == props.row
-                ? props.text[props.index]
-                : props.try
-                ? props.try[props.index]
-                : ''}
-        </div>
-    );
+  return (
+    <div
+      className="tile"
+      style={{ '--index': props.index }}
+      id={
+        props.activeRow == props.row && props.text.length > props.index
+          ? 'active'
+          : props.activeRow > props.row
+          ? props.tries[props.index] == props.word[props.index]
+            ? 'green'
+            : props.word.includes(props.tries[props.index])
+            ? 'yellow'
+            : 'gray'
+          : {}
+      }
+    >
+      {props.activeRow == props.row
+        ? props.text[props.index]
+        : props.tries
+        ? props.tries[props.index]
+        : ''}
+    </div>
+  );
 }
 
 function Row(props) {
-    return (
-        <div className="row">
-            {Array(5)
-                .fill(null)
-                .map((_, index) => (
-                    <Tile
-                        key={index}
-                        row={props.index}
-                        index={index}
-                        activeRow={props.activeRow}
-                        text={props.text}
-                        try={props.tries[props.index]}
-                        word={props.word}
-                    />
-                ))}
-            {props.current}
-        </div>
-    );
+  return (
+    <div className="row">
+      {Array(5)
+        .fill(null)
+        .map((_, index) => (
+          <Tile
+            key={index}
+            row={props.index}
+            index={index}
+            activeRow={props.activeRow}
+            text={props.text}
+            tries={props.tries[props.index]}
+            word={props.word}
+          />
+        ))}
+      {props.current}
+    </div>
+  );
 }
 
 export default function Board(props) {
-    return (
-        <motion.div
-            className="board"
-            animate={{ scale: [0, 1.1, 1] }}
-            transition={{
-                duration: 0.3,
-                ease: 'easeOut',
-                times: [0, 0.8, 1],
-            }}
-            initial={{ scale: 0 }}
-        >
-            {Array(6)
-                .fill(null)
-                .map((_, index) => (
-                    <Row
-                        key={index}
-                        index={index}
-                        activeRow={props.activeRow}
-                        text={props.text}
-                        tries={props.tries}
-                        word={props.word}
-                    />
-                ))}
-        </motion.div>
-    );
+  return (
+    <motion.div
+      className="board"
+      animate={{ scale: [0, 1.1, 1] }}
+      transition={{
+        duration: 0.3,
+        ease: 'easeOut',
+        times: [0, 0.8, 1],
+      }}
+      initial={{ scale: 0 }}
+    >
+      {Array(6)
+        .fill(null)
+        .map((_, index) => (
+          <Row
+            key={index}
+            index={index}
+            activeRow={props.activeRow}
+            text={props.text}
+            tries={props.tries}
+            word={props.word}
+          />
+        ))}
+    </motion.div>
+  );
 }
--- components/Keyboard.js
+++ components/Keyboard.js
@@ -5,69 +5,65 @@ const second = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'];
 const third = ['ENTER', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '⌫'];
 
 function Tile(props) {
-    return (
-        <motion.div
-            className="key"
-            id={
-                props.activeKey.toLowerCase() === props.code.toLowerCase()
-                    ? 'active'
-                    : ''
-            }
-        >
-            {props.text}
-        </motion.div>
-    );
+  return (
+    <motion.div
+      className="key"
+      id={
+        props.activeKey.toLowerCase() === props.code.toLowerCase()
+          ? 'active'
+          : ''
+      }
+    >
+      {props.text}
+    </motion.div>
+  );
 }
 
 function Row(props) {
-    return (
-        <div className="row">
-            {props.row === 0
-                ? first.map((letter) => (
-                      <Tile
-                          code={letter}
-                          text={letter}
-                          key={letter}
-                          activeKey={props.activeKey}
-                      />
-                  ))
-                : props.row === 1
-                ? second.map((letter) => (
-                      <Tile
-                          code={letter}
-                          text={letter}
-                          key={letter}
-                          activeKey={props.activeKey}
-                      />
-                  ))
-                : third.map((letter) => (
-                      <Tile
-                          code={letter != '⌫' ? letter : 'Backspace'}
-                          text={letter}
-                          key={letter}
-                          activeKey={props.activeKey}
-                      />
-                  ))}
-        </div>
-    );
+  return (
+    <div className="row">
+      {props.row === 0
+        ? first.map((letter) => (
+            <Tile
+              code={letter}
+              text={letter}
+              key={letter}
+              activeKey={props.activeKey}
+            />
+          ))
+        : props.row === 1
+        ? second.map((letter) => (
+            <Tile
+              code={letter}
+              text={letter}
+              key={letter}
+              activeKey={props.activeKey}
+            />
+          ))
+        : third.map((letter) => (
+            <Tile
+              code={letter != '⌫' ? letter : 'Backspace'}
+              text={letter}
+              key={letter}
+              activeKey={props.activeKey}
+            />
+          ))}
+    </div>
+  );
 }
 
 function Keyboard(props) {
-    return (
-        <>
-            <div className="keyboard">
-                {Array(3)
-                    .fill(null)
-                    .map((_, index) => (
-                        <Row
-                            key={index}
-                            row={index}
-                            activeKey={props.activeKey}
-                        />
-                    ))}
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="keyboard">
+        {Array(3)
+          .fill(null)
+          .map((_, index) => (
+            <Row key={index} row={index} activeKey={props.activeKey} />
+          ))}
+      </div>
+    </>
+  );
 }
 
 export default Keyboard;
